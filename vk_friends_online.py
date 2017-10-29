69db175a853fbcc73a6672fdfe7a9b198637b469
xdass <420600@gmail.com>
2017-10-29 15:12:15 +0500

    Init commit

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,29 +1,49 @@
 import vk
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6238133  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    #login = input('Введите ваш логин VK: ')
+    login = 'xbase@uralweb.ru'
+    return login
 
 
 def get_user_password():
-    pass
+    #password = login = getpass.getpass('Введите пароль: ')
+    #password= input('Введите пароль: ')
+    password = '420600base'
+    return password
 
 
 def get_online_friends(login, password):
+    code = """
+    var online =  API.friends.getOnline({"user_id": 5088491});
+
+    var result = [];
+    var user = "";
+    while (online.length > 0) {
+    
+    result.push(API.users.get({"user_ids": online.shift()}));
+    };
+    
+    return result;
+    """
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='offline, friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    return api.execute(code=code)
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Пользователи онлайн: ')
+    for user in friends_online:
+        print(user[0]['first_name'], user[0]['last_name'])
 
 if __name__ == '__main__':
     login = get_user_login()
